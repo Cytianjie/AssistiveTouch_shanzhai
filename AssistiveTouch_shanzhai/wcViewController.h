2bfc228fe6226d9ebc09c4b6a145d8fa3aeb473b
Cytianjie <hesongchuanmei@hesongchuanmeideMac-mini.local>
2015-03-25 17:29:01 +0800

    11
    qq

---
+++ AssistiveTouch_shanzhai/wcViewController.h
@@ -0,0 +1,13 @@
+//
+//  wcViewController.h
+//  AssistiveTouch_shanzhai
+//
+//  Created by 和颂传媒 on 15/3/25.
+//  Copyright (c) 2015年 beijing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wcViewController : UIViewController
+
+@end
